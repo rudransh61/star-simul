51e7ef6317a57bda9bcc1a5ad1a7ff0600fcf704
Rudransh Bhardwaj <aarti19830@gmail.com>
2024-06-12 17:13:50 +0530

    daalo , done effect made , star forming and blasiting

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"image/color"
 	"math"
 	"math/rand"
@@ -24,27 +23,27 @@ var (
 	balls           []*PVEBody // Using PVEBody instead of rigidbody.RigidBody
 	dt              = 0.1
 	ticker          *time.Ticker
-	initialInterval = time.Second / 5 // Initial interval for adding particles
-	center          vector.Vector     // Center of the screen
+	initialInterval = time.Second / 10 // Initial interval for adding particles
+	center          vector.Vector      // Center of the screen
 )
 
 const (
-	Mass       = 2
+	Mass       = 1
 	Shape      = "Circle"
-	Radius     = 5     // Tiny particles
-	Friction   = 1     // Friction coefficient
-	Gravity    = 100   // Strength of gravity towards the center
-	InitRadius = 300.0 // Initial radius of particle distribution
+	Radius     = 2      // Tiny particles
+	Friction   = 0.899  // Friction coefficient
+	Gravity    = 50     // Strength of gravity towards the center
+	InitRadius = 1000.0 // Initial radius of particle distribution
 )
 const (
-	Viscosity     = 0.1 // Adjust the viscosity coefficient for desired effect
-	HeatTransfer  = 0.1 // Rate of heat transfer between particles
-	HeatThreshold = 0.5 // Threshold for heat-based repulsive force
+	Viscosity     = 0.999 // Adjust the viscosity coefficient for desired effect
+	HeatTransfer  = 0.1   // Rate of heat transfer between particles
+	HeatThreshold = 0.5   // Threshold for heat-based repulsive force
 )
 
 var (
 	particlesAdded = 0
-	maxParticles   = 100 // Maximum number of particles to add
+	maxParticles   = 100000 // Maximum number of particles to add
 )
 
 func update() error {
@@ -55,53 +54,49 @@ func update() error {
 			addParticle()
 			addParticle()
 			addParticle()
-			particlesAdded++
-			fmt.Println("Particles added: ", particlesAdded)
+			addParticle()
+			addParticle()
+			addParticle()
+			addParticle()
+			addParticle()
+			addParticle()
+			addParticle()
+			addParticle()
+			addParticle()
+			addParticle()
+			addParticle()
 		default:
 		}
 	}
-	for _, ball1 := range balls {
-		for _, ball2 := range balls {
-			if ball1 != ball2 {
-				// Calculate distance and direction between particles
-				distance := ball1.Position.Sub(ball2.Position)
-				distanceMagnitude := distance.Magnitude()
-				moveDirection := distance.Normalize()
-
-				// Calculate repulsive force magnitude
-				repulsiveForceMagnitude := 10 / (distanceMagnitude * distanceMagnitude) // Adjust the factor for desired repulsive effect
-
-				// Apply repulsive force to velocities
-				repulsiveForce := moveDirection.Scale(repulsiveForceMagnitude)
-				ball1.Velocity = ball1.Velocity.Add(repulsiveForce.Scale(dt / ball1.Mass))
-
-				// Transfer heat between particles
-				heatTransfer := (ball1.Heat - ball2.Heat) * HeatTransfer
-				ball1.Heat -= heatTransfer
-				ball2.Heat += heatTransfer
-			}
-		}
-	}
-
 	for _, ball := range balls {
 		// Apply gravity towards the center
 		gravity := center.Sub(ball.Position).Normalize().Scale(Gravity)
 		ApplyForcePVE(ball, gravity, dt)
 
 		// Apply friction
-		// ball.Velocity = ball.Velocity.Scale(Friction)
+		ball.Velocity = ball.Velocity.Scale(Friction)
 
 		// Apply viscosity
-		viscosityForce := ball.Velocity.Scale(-Viscosity)
-		ApplyForcePVE(ball, viscosityForce, dt)
+		// ball.Velocity = ball.Velocity.Scale(-Viscosity)
+		// ApplyForcePVE(ball, viscosityForce, dt)
 	}
+
+	// // Add gravitation
+	// for i := 0; i < len(balls); i++ {
+	// 	for j := i + 1; j < len(balls); j++ {
+	// 		distance := vector.Distance(balls[i].Position, balls[j].Position)
+	// 		gravitationalForce := balls[j].Position.Sub(balls[i].Position).Normalize().Scale(0.000001 * 0.000001 * 0.0000000000000000000000001 / (distance + 1500))
+	// 		ApplyForcePVE(balls[i], gravitationalForce, dt)
+	// 		ApplyForcePVE(balls[j], gravitationalForce.Scale(-1), dt)
+	// 	}
+	// }
 
 	// Resolve collisions and apply repulsive force to simulate thermal pressure
 	for i := 0; i < len(balls); i++ {
 		for j := i + 1; j < len(balls); j++ {
 			if collision.CircleCollided(balls[i].RigidBody, balls[j].RigidBody) {
-				resolveCollision(balls[i].RigidBody, balls[j].RigidBody)
-				collision.BounceOnCollision(balls[i].RigidBody, balls[j].RigidBody, 0.9)
+				// collision.BounceOnCollision(balls[i].RigidBody, balls[j].RigidBody, 1.0)
+				resolveCollision(balls[i].RigidBody, balls[j].RigidBody, balls)
 			}
 		}
 	}
@@ -109,28 +104,32 @@ func update() error {
 	return nil
 }
 
-func resolveCollision(ball1, ball2 *rigidbody.RigidBody) {
+func resolveCollision(ball1, ball2 *rigidbody.RigidBody, balls []*PVEBody) {
 	distance := ball1.Position.Sub(ball2.Position)
 	distanceMagnitude := distance.Magnitude()
 	minimumDistance := ball1.Radius + ball2.Radius
 
-	if distanceMagnitude-(Radius/2) < minimumDistance {
+	if distanceMagnitude < minimumDistance {
 		moveDirection := distance.Normalize()
-		moveAmount := minimumDistance - distanceMagnitude
+		overlap := minimumDistance - distanceMagnitude
 
-		// Move balls apart to resolve overlap
-		moveVector1 := moveDirection.Scale(moveAmount / 2)
-		moveVector2 := moveDirection.Scale(-moveAmount / 2)
-		ball1.Position = ball1.Position.Add(moveVector1)
-		ball2.Position = ball2.Position.Add(moveVector2)
-
-		// Apply a repulsive force
-		repulsiveForceMagnitude := moveAmount * 1 / (distanceMagnitude * distanceMagnitude) // Adjust the factor for desired repulsive effect
+		// Calculate the repulsive force magnitude based on the overlap
+		mag := 10.0
+		if len(balls) >= 1000 {
+			mag = 350
+		}
+		repulsiveForceMagnitude := overlap * mag // Adjust this factor as needed for desired effect
 		repulsiveForce := moveDirection.Scale(repulsiveForceMagnitude)
 
-		// Apply repulsive force to velocities
+		// Apply the repulsive force to the velocities of the balls
 		ball1.Velocity = ball1.Velocity.Add(repulsiveForce.Scale(dt / ball1.Mass))
 		ball2.Velocity = ball2.Velocity.Add(repulsiveForce.Scale(-dt / ball2.Mass))
+
+		// Adjust positions slightly to avoid sticking
+		correctionFactor := 0.5 // Adjust this factor as needed for desired effect
+		positionCorrection := moveDirection.Scale(correctionFactor * overlap)
+		ball1.Position = ball1.Position.Add(positionCorrection)
+		ball2.Position = ball2.Position.Sub(positionCorrection)
 	}
 }
 
@@ -157,7 +156,7 @@ func main() {
 	ticker = time.NewTicker(initialInterval)
 
 	// Initialize with a few particles
-	initializeBalls(10)
+	initializeBalls(200)
 
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		panic(err)
@@ -181,7 +180,7 @@ func initializeBalls(n int) {
 				Radius:    Radius,
 				IsMovable: true,
 			},
-			Heat: 1.0, // Set initial heat value
+			Heat: 100.0, // Set initial heat value
 		}
 		balls = append(balls, ball)
 	}
@@ -201,7 +200,7 @@ func addParticle() {
 			Radius:    Radius,
 			IsMovable: true,
 		},
-		Heat: 1.0, // Set initial heat value
+		Heat: 100.0, // Set initial heat value
 	}
 	balls = append(balls, ball)
 }
@@ -223,8 +222,6 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 func ApplyForcePVE(body *PVEBody, force vector.Vector, dt float64) {
 	if body.IsMovable {
 		// Use Newton's second law: F = ma -> a = F/m
-		// rb.Force = rb.Force.Add(force)
-
 		body.Force = force
 		acceleration := body.Force.Scale(1 / body.Mass)
 
@@ -233,5 +230,7 @@ func ApplyForcePVE(body *PVEBody, force vector.Vector, dt float64) {
 
 		// Update position using velocity and time step
 		body.Position = body.Position.Add(body.Velocity.Scale(dt))
+
+		body.Heat = body.Velocity.Scale(0.5).Magnitude()
 	}
 }
